9640af40045164791a4f441300467f79fc81007b
Chris <christopher.mcnally@hscni.net>
2021-08-25 14:27:16 +0100

    DA-49

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 ####### SETUP  ####### 
 
 # Libraries
+library(shinydashboard)
 library(tidyverse)
 library(plyr)
 library(lubridate)
@@ -9,6 +10,11 @@ library(rworldmap)
 
 # Source scripts
 source("./azure_functions.R")
+
+# Load data
+locations <- getTable("Locations")
+collectclosecontacts <- getTable("CollectContactsCalls")
+cases <- getTable("cases")
 
 function(input, output, session) {
   
@@ -32,6 +38,8 @@ function(input, output, session) {
   green.countries <- as.data.frame(green.countries)
   colnames(green.countries) <- "country"
   green.countries$status <- "Green"
+  green.countries <- lapply(green.countries, str_trim) #Trim whitespace
+  green.countries <- as.data.frame(green.countries)
   
   # Web scraping and defining red countries
   
@@ -47,66 +55,93 @@ function(input, output, session) {
   red.countries <- str_split(red.countries, pattern = "\n\t") #Split single string into each country 
   red.countries <- as.data.frame(red.countries)
   colnames(red.countries) <- "country"
-  red.countries <- gsub("\\s*\\([^\\)]+\\)","", as.character(red.countries$country)) #Remove brackets and everything in them
+  red.countries <- gsub("\\s*\\([^\\)]+\\)", " ", as.character(red.countries$country)) #Remove brackets and everything in them
   red.countries <- str_remove(red.countries, "[\n]")
   red.countries <- as.data.frame(red.countries)
   colnames(red.countries) <- "country"
   red.countries$status <- "Red"
+  red.countries <- lapply(red.countries, str_trim) #Trim whitespace 
+  red.countries <- as.data.frame(red.countries)
   
-  # Defining amber countries
-  CTA <- as.data.frame(c("United Kingdom", "Isle of Man", "Ireland", "Guernsey", "Jersey")) #Define Common Travel Area
+  # Define Common Travel Area
+  CTA <- as.data.frame(c("England", "Scotland", "Wales", "Isle of Man", "Ireland", "Guernsey", "Jersey")) 
   CTA$status <- "Green"
   colnames(CTA) <- c("country", "status")
   
   ## Define all countries in world
-  amber.countries <- countryExData[ , 2] #rworldmap package which lists all countries 
-  amber.countries <- gsub("Dem. Rep. Congo","Democratic Republic of Congo", amber.countries, fixed = T) 
-  amber.countries <- gsub("Rep.","Republic", amber.countries, fixed = T) 
-  amber.countries <- gsub("Viet Nam","Vietnam", amber.countries, fixed = T) 
-  amber.countries <- as.data.frame(amber.countries)
-  colnames(amber.countries) <- "country"
+  all.countries <- countryExData[ , 2] #rworldmap package which lists all countries 
+  all.countries <- gsub("Dem. Rep. Congo","Democratic Republic of Congo", all.countries, fixed = T) 
+  all.countries <- gsub("Rep.","Republic", all.countries, fixed = T) 
+  all.countries <- gsub("Viet Nam","Vietnam", all.countries, fixed = T) 
+  all.countries <- as.data.frame(all.countries)
+  all.countries$status <- NA
+  colnames(all.countries) <- c("country", "status")
   
   ## Amber countries are countries not on red or green lists or CTA
   non.amber.countries <- rbind(red.countries, green.countries, CTA)
   
   ## Remove countries that appear in green and red lists and CTA
-  amber.countries <- anti_join(amber.countries, non.amber.countries)
+  amber.countries <- anti_join(all.countries, non.amber.countries, by = "country")
   amber.countries$status <- "Amber"
-  
-  
   
   # Country Status data frame
   
-  all.countries <- rbind(amber.countries, non.amber.countries)
-  all.countries <- all.countries[order(all.countries$country), ] 
-  present.country.status <- all.countries$status
+  all.countries.status <- rbind(amber.countries, non.amber.countries)
+  all.countries.status <- all.countries.status[order(all.countries.status$country), ] 
+  current.country.status <- all.countries.status$status
   
   
-  country.status <- data.frame(matrix(NA, nrow = 200, ncol = 52))
-  row.names(country.status) <- all.countries$country
-  
+  country.status <- data.frame(matrix(NA, nrow = 189, ncol = 52))
   
   epiweek <- (paste0("Epiweek", 1:52))
   colnames(country.status) <- epiweek
+  old.epiweeks <- (paste0("Epiweek", 1:31))
   
   current.epiweek <- paste0("Epiweek", strftime(Sys.Date(), format = "%V"))
+  
+  # Assign this weeks data
   status.assignment <- country.status %>% 
     select(current.epiweek) 
-  status.assignment[,1] <- present.country.status
+  status.assignment[,1] <- current.country.status
   country.status <- replace(country.status, current.epiweek, status.assignment[,1])
+  country.status$Epiweek32 <- country.status$Epiweek34
   country.status$Epiweek33 <- country.status$Epiweek34
-  country.status$country <- all.countries$country
-  
+  country.status$country <- all.countries.status$country
   
   write.csv(country.status, "country.status.backup.csv")
   
+  
+  
+  # Old Country Status lists (Prior to Changes Implemented August 8th 2021)
+  
+  ## Green
+  green.changes <- data.frame("country" = c("Austria", "Germany", "Latvia", "Norway", "Romania", "Slovenia", "Slovakia")) #These countries were added to green list August 8th 21
+  old.green.countries <- anti_join(x = green.countries, y = green.changes, by = "country")
+  
+  ## Red
+  red.changes <- data.frame("country" = c("Georgia", "La Reunion", "Mayotte", "Mexico")) #These countries were added to red list August 8th 21
+  old.red.countries <- anti_join(x = red.countries, y = red.changes, by = "country")
+  
+  ## Amber
+  old.non.amber.countries <- rbind(old.red.countries, old.green.countries, CTA)
+  old.amber.countries <- anti_join(all.countries, old.non.amber.countries, by = "country")
+
+  
+  ## Old Country Status data frame
+  old.all.countries.status <- rbind(old.amber.countries, old.non.amber.countries)
+  missed.countries <- anti_join(all.countries.status, old.all.countries.status, by = "country")
+  old.amber.countries <- rbind(old.amber.countries, missed.countries)
+  old.amber.countries$status <- "Amber"
+  
+  old.all.countries.status <- rbind(old.amber.countries, old.non.amber.countries)
+  old.all.countries.status <- old.all.countries.status[order(old.all.countries.status$country), ] 
+  old.country.status <- old.all.countries.status$status
+  
+  # Add old cases (pre-august 8th, pre epiweek 32) to country.status data frame
+  country.status <- replace(country.status, old.epiweeks, old.country.status) 
+  
+  ## Pivot Countries
   pivot.countries <- pivot_longer(country.status, !country, names_to = "Epiweek", values_to = "Status")
-  
-  # Load data
-  locations <- getTable("Locations")
-  collectclosecontacts <- getTable("CollectContactsCalls")
-  cases <- getTable("cases")
-  
   
   # Define travellers
   
@@ -116,6 +151,7 @@ function(input, output, session) {
     left_join(collectclosecontacts, by = c("CollectCallId" = "Id")) %>%
     left_join(cases, by = "CaseNumber") %>%
     select(CountriesVisited.x, WhenDidYouLeaveNorthernIreland, WhenDidYouReturnToNorthernIreland, CaseNumber, FirstName, LastName, Gender.x, CreatedOn) %>% 
+    filter(CreatedOn >= "2021-01-01") %>% 
     mutate(Epiweek = paste0("Epiweek", strftime(WhenDidYouReturnToNorthernIreland, format = "%V")))
   
   ## Tidy old data
@@ -125,15 +161,86 @@ function(input, output, session) {
   travellers$CountriesVisited.x <- gsub("R.O.I.", "Ireland", travellers$CountriesVisited.x)
   travellers$CountriesVisited.x <- gsub("Republic of Ireland", "Ireland", travellers$CountriesVisited.x)
   travellers$CountriesVisited.x <- gsub("REPUBLIC OF IRELAND", "Ireland", travellers$CountriesVisited.x)
-  
-  time.travellers.check <- travellers %>% 
-    filter(WhenDidYouReturnToNorthernIreland >= Sys.Date())
-
 
   ## Link data to country status 
   travellers <- left_join(travellers, pivot.countries, by = c("Epiweek" = "Epiweek", "CountriesVisited.x" = "country"))
   
-  startdate <- as.Date("2021-08-16")
+  
+  # Make Graphs
+  
     
-    ####### HOME ####### 
+  ####### HOME #######
+  output$total_cases <- renderInfoBox({
+    infoBox(
+      "Total Reported Cases from International Travellers", 
+      paste0(nrow(filter(travellers))), 
+      icon = icon("globe-europe"), 
+      color ="light-blue")
+  })
+  
+  output$cases_this_week <- renderInfoBox({
+    infoBox(
+      "Reported Cases This Week from International Travellers", 
+      paste0(nrow(filter(travellers,
+                         Epiweek == current.epiweek))), 
+      icon = icon("globe-europe"), 
+      color = "light-blue")
+  })
+  
+  output$total_green_cases <- renderInfoBox({
+    infoBox(
+      "Total Reported Cases from Green Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Green"))), 
+      icon = icon("virus"), 
+      color ="green")
+  })
+  
+  output$green_cases_this_week <- renderInfoBox({
+    infoBox(
+      "Cases This Week from Green Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Green",
+                         Epiweek == current.epiweek))), 
+      icon = icon("virus"), 
+      color = "green")
+  })
+  
+  output$total_amber_cases <- renderInfoBox({
+    infoBox(
+      "Total Reported Cases from Amber Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Amber"))), 
+      icon = icon("viruses"), 
+      color ="yellow")
+  })
+  
+  output$amber_cases_this_week <- renderInfoBox({
+    infoBox(
+      "Cases This Week from Amber Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Amber",
+                         Epiweek == current.epiweek))), 
+      icon = icon("viruses"), 
+      color = "yellow")
+  })
+  
+  output$total_red_cases <- renderInfoBox({
+    infoBox(
+      "Total Reported Cases from Red Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Red"))), 
+      icon = icon("virus"), 
+      color ="red")
+  })
+  
+  output$red_cases_this_week <- renderInfoBox({
+    infoBox(
+      "Cases This Week from Red Countries", 
+      paste0(nrow(filter(travellers,
+                         Status == "Red",
+                         Epiweek == current.epiweek))), 
+      icon = icon("virus"), 
+      color = "red")
+  })
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,64 @@
-library(shiny)
+######## SETUP ######## 
 
-####### HOME  ####### 
-tableOutput('green.countries')
+# Libraries
+library(shinydashboard)
+library(shinycssloaders)
 
+######## SPINNER ########
+options(spinner.color = "#0275D8", spinner.color.background="#ffffff", spinner.size =2)
+
+######## UI ######## 
+
+ui <- dashboardPage(
+  
+  dashboardHeader(title = "International Travel Dashboard"),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("bookmark"))
+    )
+  ),
+
+dashboardBody(
+  
+  tabItems(
+    
+    #--------------HOME--------------
+    
+    tabItem(
+      tabName = "home",
+      
+      fluidRow(
+        box(
+          width = 12,
+          status = "primary",
+          solidHeader = TRUE,
+          title ="Welcome to the International Travel Dashboard.",
+          p("This dashboard has been built to aid the CTC Data Management Team / Surveillance Team to montior COVID-19 cases related to international travel in 2021"),
+          p(strong("Please Note:")," The free text inputs of country entries is yet to be addressed, therefore, some countries will be omitted temporarily")
+        )
+      ),
+      
+      fluidRow(
+        withSpinner(infoBoxOutput("total_cases", width = 6), type = 2),
+        infoBoxOutput("cases_this_week", width = 6)
+      ),
+      
+        fluidRow(
+        infoBoxOutput("total_green_cases", width = 6),
+        infoBoxOutput("green_cases_this_week", width = 6)
+       ),
+      
+      fluidRow(
+        infoBoxOutput("total_amber_cases", width = 6),
+        infoBoxOutput("amber_cases_this_week", width = 6)
+      ),
+      
+      fluidRow(
+        infoBoxOutput("total_red_cases", width = 6),
+        infoBoxOutput("red_cases_this_week", width = 6)
+      )
+      )
+    )
+  )
+)
